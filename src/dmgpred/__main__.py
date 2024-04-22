9484961926c9aacb0a5b88656a76c1e7bc7c9ff2
Moritz Mistol <usort@student.kit.edu>
2024-04-22 17:05:48 +0200

    use data path variable

--- src/dmgpred/__main__.py
+++ src/dmgpred/__main__.py
@@ -9,17 +9,23 @@ from dmgpred.evaluate import evaluate
 from dmgpred.featurize import featurize
 from dmgpred.train import train
 
-TEST_VALUES_PATH = "./data/test_values.csv"
-TRAIN_VALUES_PATH = "./data/train_values.csv"
-TRAIN_LABELS_PATH = "./data/train_labels.csv"
+DATA_PATH = "./data/"
+TEST_VALUES_PATH = f"{DATA_PATH}/test_values.csv"
+TRAIN_VALUES_PATH = f"{DATA_PATH}/train_values.csv"
+TRAIN_LABELS_PATH = f"{DATA_PATH}/train_labels.csv"
 SUBMISSION_PATH = "./output/Mandalorians_prediction.csv"
 
 
 def main():
     """Run Prediction Pipeline."""
+    # a simple timer, could use TQDM later on for progress bars
     start = time.perf_counter()
+
     X_test = pd.read_csv(TEST_VALUES_PATH)
     X_train = pd.read_csv(TRAIN_VALUES_PATH)
+
+    # need building id as index here,
+    # otherwise it is interpreted as multi-output classification
     y_train = pd.read_csv(TRAIN_LABELS_PATH, index_col="building_id")
 
     X_train, X_test = clean(X_train, X_test)
@@ -27,6 +33,7 @@ def main():
     model = train(X_train, y_train)
 
     # TODO: implement evaluation with custom metrics
+    # e.g. with command-line arguments, or later with Hydra
     score = evaluate(model, X_train, y_train)
     print(f"Matthews Correlation Coefficient: {score: .4f}")
 
