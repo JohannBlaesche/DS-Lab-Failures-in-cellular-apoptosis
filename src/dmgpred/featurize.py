10747f39009115b88d88ac9b1d4f781e1905ae39
Moritz Mistol <usort@student.kit.edu>
2024-05-02 16:29:14 +0200

    delete unused code

--- src/dmgpred/featurize.py
+++ src/dmgpred/featurize.py
@@ -42,17 +42,6 @@ def featurize_single(X: pd.DataFrame) -> pd.DataFrame:
     pd.DataFrame
         DataFrame with featurization applied.
     """
-    # X = encode_categoricals(X)
-    return X
-
-
-def encode_categoricals(X: pd.DataFrame) -> pd.DataFrame:
-    """Encode categorical columns using one-hot encoding."""
-    object_cols = X.select_dtypes(include="category").columns
-    # object_cols = np.append(object_cols, "count_families")
-    for col in object_cols:
-        le = OneHotEncoder()
-        X[col] = le.fit_transform(X[col])
     return X
 
 
