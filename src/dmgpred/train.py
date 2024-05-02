c6dd149f02e5c42536feb927716627110356b65e
Moritz Mistol <usort@student.kit.edu>
2024-05-02 16:42:17 +0200

    refactor pipeline into its own function

--- src/dmgpred/train.py
+++ src/dmgpred/train.py
@@ -1,6 +1,7 @@
 """Training step in the pipeline."""
 
 import pandas as pd
+from sklearn.dummy import DummyClassifier
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.pipeline import Pipeline
 
@@ -8,20 +9,29 @@ from dmgpred.cleaning import get_normalization_pipeline
 from dmgpred.featurize import get_encoder
 
 
-def train(X_train: pd.DataFrame, y_train: pd.DataFrame):
-    """Run the training step."""
+def get_pipeline(X: pd.DataFrame, clf=None):
+    """Return the training pipeline."""
+    if clf is None:
+        clf = DummyClassifier(strategy="most_frequent")
     normalizer = get_normalization_pipeline()
-    encoder = get_encoder(X_train)
-    model = Pipeline(
+    encoder = get_encoder(X)
+    return Pipeline(
         [
             ("normalizer", normalizer),
-            ("encode", encoder),
-            (
-                "clf",
-                RandomForestClassifier(max_depth=3, n_estimators=50),
-            ),
+            ("encoder", encoder),
+            ("clf", clf),
         ],
         verbose=False,
     )
-    model.fit(X_train, y_train)
-    return model
+
+
+def train(X_train: pd.DataFrame, y_train: pd.DataFrame):
+    """Train the model on the full dataset.
+
+    This model is used to predict the damage grade of the test data.
+    A seperate evaluation is done using cross-validation.
+    """
+    clf = RandomForestClassifier(n_estimators=50, max_depth=3)
+    pipe = get_pipeline(X_train, clf=clf)
+    pipe.fit(X_train, y_train)
+    return pipe
