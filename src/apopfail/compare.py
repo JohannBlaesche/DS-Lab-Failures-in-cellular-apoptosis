d24bfa9e062a3395002eb669bf2a8962a8cb2e31
Moritz Mistol <usort@student.kit.edu>
2024-07-01 12:23:41 +0200

    add file stub

---
+++ src/apopfail/compare.py
@@ -0,0 +1,11 @@
+"""Model comparison."""
+
+
+def compare_occ_models():
+    """Compare One-Class Classification models."""
+    pass
+
+
+def build_model_list():
+    """Build a list of models."""
+    pass
